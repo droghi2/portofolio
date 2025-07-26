52793f9f07cb1cd19598286f2091d47b4dcc299d
Bordei Matei <themaftei@gmail.com>
2025-07-26 17:52:07 +0300

    added new photos

--- main.js
+++ main.js
@@ -29,8 +29,8 @@ let clipNames = [
 ];
 let projects = [
   
-  { images: ['textures/FoggiMainArea.png', 'textures/SnowArea.png', 'textures/CaveEntrance.png'], url: 'http://example.com/project1' },
-  { images: ['textures/WholeMainArea.png', 'textures/SnowTODesert.png', 'textures/CaveWithLake.png'], url: 'http://example.com/project2' },
+  { images: ['textures/Main/Main_CloudyGrass.png', 'textures/SnowArea.png', 'textures/CaveEntrance.png'], url: 'http://example.com/project1' },
+  { images: ['textures/Main/Main_WholeMainArea.png', 'textures/SnowTODesert.png', 'textures/CaveWithLake.png'], url: 'http://example.com/project2' },
   // { images: ['test.png', 'test2.png'], url: 'http://example.com/project3' },
   // { images: ['project4_1.jpg', 'project4_2.jpg'], url: 'http://example.com/project4' },
   // { images: ['project5_1.jpg', 'project5_2.jpg'], url: 'http://example.com/project5' },  
